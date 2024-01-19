f9a6a59aca00368ecb8d2a37f3a34e721ad11c03
Mary <mary@moralis.io>
2024-01-19 17:39:38 +0100

    added tests

--- cypress/e2e/task1.spec.cy.js
+++ cypress/e2e/task1.spec.cy.js
@@ -1,9 +1,18 @@
 describe('Login Module', ()=>{
-  it('login as a standard user', () => {
+  beforeEach(()=>{
     cy.visit('https://www.saucedemo.com');
+  })
+  it('login with valid details', () => {
     cy.get('#user-name').type('standard_user');
     cy.get('#password').type('secret_sauce');
     cy.get('#login-button').click();
     cy.get('.shopping_cart_link').should('be.visible');
+  })
+
+  it('login with invalid details', () => {
+    cy.get('#user-name').type('standard');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get('[data-test=error]').should('be.visible').and('have.text', 'Epic sadface: Username and password do not match any user in this service')
   })
 })
