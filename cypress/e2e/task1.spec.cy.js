35a5432997b2a66c1ab8d79881b795487a41b568
Mary <mary@moralis.io>
2024-01-19 12:10:09 +0100

    made some changes

--- cypress/e2e/task1.spec.cy.js
+++ cypress/e2e/task1.spec.cy.js
@@ -1,8 +1,9 @@
-
-it('login as a standard user', () => {
-  cy.visit('https://www.saucedemo.com');
-  cy.get('#user-name').type('standard_user');
-  cy.get('#password').type('secret_sauce');
-  cy.get('#login-button').click();
-  cy.get('.shopping_cart_link').should('be.visible');
+describe('Login Module', ()=>{
+  it('login as a standard user', () => {
+    cy.visit('https://www.saucedemo.com');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get('.shopping_cart_link').should('be.visible');
+  })
 })
