20a89262b2f6f0c6292e5bdda1b171bfb41b71ff
Mary <mary@moralis.io>
2024-01-29 12:37:30 +0100

    Odd or even task

---
+++ module3-JavaScript/task6.js
@@ -0,0 +1,14 @@
+/**
+ *  Task: Odd or even?
+    Write a Javascript program that checks if a given number, number is odd or even. 
+    If the number is odd, print to the console: number is odd. If  the number is even, print to the console: number is even 
+ */
+
+
+const numberOfStudent = 49;
+
+if(numberOfStudent % 2 === 0){
+    console.log('Number is even');
+} else {
+    console.log('Number is odd');
+}
