22f410caf701244b31a608c18714a12484a18eaa
Mary <mary@moralis.io>
2024-01-31 15:31:15 +0100

    task9 and task10

---
+++ module3-JavaScript/task10.js
@@ -0,0 +1,14 @@
+/**
+ * Task: Area Of a Rectangle
+    Write JavaScript function, calculateAreaOfRectangle. The function should
+    Take two parameters, length and width. 
+    Multiplies the length and width
+    Returns the area of the rectangle
+ */
+
+
+function calculateAreaOfRectangle(length, width){
+    const areaOfRectangle = length * width;
+    return areaOfRectangle;
+}
+calculateAreaOfRectangle(4, 6);
---
+++ module3-JavaScript/task9.js
@@ -0,0 +1,16 @@
+/**
+ * Task: Area Of a Rectangle
+    Write JavaScript function, calculateAreaOfRectangle. The function should
+    Take two parameters, length and width. 
+    Multiply the length and width
+    Log the product to the console
+ */
+
+
+function calculateAreaOfRectangle(length, width){
+    console.log(`The area of the rectangle is ${length * width}.`);
+}
+
+calculateAreaOfRectangle(4, 22);
+
+// onuorahmary@gmail.com
