2fb861f73cbc18bbbc2baad4ea34993f5a50e9a5
Mary <mary@moralis.io>
2024-02-02 09:04:51 +0100

    task11 and task 12

---
+++ module3-JavaScript/task11&12.js
@@ -0,0 +1,21 @@
+/**
+ *  Task 11: Create an Array of Best Friends
+    A JavaScript array is basically an ordered list of comma-separated values. Each value is called an element.
+    Create a JavaScript array for top 5 friends.
+
+    Steps: 
+    Declare a variable, friends and assign it an array [ ]
+    Add 5 elements in the array (Each element should be of type string. Remember to separate each element by a comma.)
+    Log your friends array to the console and take a look.
+
+    Task 12: Pick your Best Friend
+    Elements of an array are index based. 
+    From your array of best created in the previous task, choose your best friend based on their index in the array.  
+
+ */
+
+const friends = ['Joy', 'Etiena', 'Ovo', 'Love', 'Ifeoma'];
+
+const bestFriend = friends[3];
+
+console.log(bestFriend);
