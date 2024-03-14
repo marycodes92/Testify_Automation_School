cd59b1f5524dfc8fbf5dd126e45c87ad8678afc9
Mary <mary@moralis.io>
2024-03-14 21:22:47 +0100

    Java task 1

---
+++ Module4/src/Tasks/Task1.java
@@ -0,0 +1,10 @@
+package Tasks;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("This is my first java task");
+        System.out.println("I am so happy writing java code");
+        System.out.println("I'm so excited to see what I'd create with Java");
+        System.out.println("I've always been scared of writing Java");
+    }
+}
