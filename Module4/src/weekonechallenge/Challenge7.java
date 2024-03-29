5f8d8b3f609736f9764e82c1d0b89829d75834c9
Mary <mary@moralis.io>
2024-03-29 12:11:32 +0100

    update

--- Module4/src/weekonechallenge/Challenge7.java
+++ Module4/src/weekonechallenge/Challenge7.java
@@ -3,11 +3,18 @@ package weekonechallenge;
 // Reverse "TestifyAutomation" using recursion in Java
 public class Challenge7 {
     public static void main(String[] args) {
-        Challenge7 reverse = new Challenge7();
-
+//        Challenge7 reverse = new Challenge7();
         String word = "TestifyAutomation";
 
-//        String result = reverse.reverseWord(word);
+        System.out.println(reverseWord(word));
     }
 
+    public static String reverseWord(String word) {
+        // Base case: if the length of the string is 0 or 1, return the string
+        if (word == null || word.length() <= 1) {
+            return word;
+        }
+
+        return reverseWord(word.substring(1)) + word.charAt(0);
+    }
 }
--- Module4/src/weekonechallenge/Challenge8.java
+++ Module4/src/weekonechallenge/Challenge8.java
@@ -1,6 +1,32 @@
 package weekonechallenge;
 
+import java.util.Arrays;
+
 // Given a collection of 1 million integers, all ranging between 1 to 9, sort them in Big O(n) time
 public class Challenge8 {
 
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 3, 1, 5, 6, 4, 7, 8, 9, 5, 3, 1, 2, 7, 8, 9, 6, 8, 6, 4, 7, 8, 9, 5, 3, 1, 2, 7, 8};
+        countingSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+
+    public static void countingSort(int[] arr) {
+        int[] count = new int[10]; //assuming the integers are in the range 1 to 9
+
+        // Count the frequency of each integer
+        for (int num : arr) {
+            count[num]++;
+        }
+
+        // Populate the sorted array using the count array
+        int index = 0;
+        for (int i = 1; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[index++] = i;
+                count[i]--;
+            }
+        }
+    }
 }
+
