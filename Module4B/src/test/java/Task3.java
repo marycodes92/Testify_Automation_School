24973ac5dc4925f5b4917ef0b379cc080e052b2a
Mary <mary@moralis.io>
2024-04-20 20:02:56 +0100

    Task 3

---
+++ Module4B/src/test/java/Task3.java
@@ -0,0 +1,15 @@
+public class Task3 {
+    /**
+     * Inspect 3 elements on the google homepage, and send screenShots pointing
+     * or highlighting the inspected elements on the developer tools.
+     * example: https://i.imgur.com/sIKrH4E.png
+     */
+}
+
+/**
+ * Below are links to the screenshots
+ * https://drive.google.com/file/d/1f-EOKeWog63eJLigHxHdQ30TMCaU00FD/view?usp=share_link
+ * https://drive.google.com/file/d/1hYse4yw5YFpQv2jp8XPTrA2oZb0uvZA-/view?usp=share_link
+ * https://drive.google.com/file/d/1_YiLq17E3q9kGmRja20ds-po1R3Fkehz/view?usp=share_link
+ */
+
