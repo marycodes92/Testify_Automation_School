400a7370736aedb5389034e1355152c794498fc7
Mary <mary@moralis.io>
2024-04-20 18:41:44 +0100

    Task 2

---
+++ Module4B/src/test/java/Task2/NavigateToGuruBank.java
@@ -0,0 +1,21 @@
+package Task2;
+
+//Create three different classes and launch any three websites of your choice from each of them
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Collections;
+
+public class NavigateToGuruBank {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/maryonuorah/Desktop/Testify Academy/Tasks/Testify_Automation_School/Module4B/src/chromedriver");
+
+        WebDriver driver = new ChromeDriver(); //laucnhes the browser
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.saucedemo.com"); //navigate to the url
+    }
+}
