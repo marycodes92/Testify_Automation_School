8fc583b13bcdf82e96629f2a99db2e6cc6d3cc30
Mary <mary@moralis.io>
2024-05-05 14:43:17 +0100

    task 14

---
+++ Module4B/src/test/java/Tasks/Task14.java
@@ -0,0 +1,28 @@
+package Tasks;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Task14 {
+    /**
+     * Navigate to https://www.toolsqa.com/
+     * close the dialogue box that pops up after a few seconds
+     * Click on the tutorial button on the top left corner of the page
+     */
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/maryonuorah/Desktop/Testify Academy/Tasks/Testify_Automation_School/Module4B/src/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        driver.get("https://www.toolsqa.com/");
+
+        driver.findElement(By.xpath("//span[contains(text(),'Tutorials')]")).click();
+    }
+}
