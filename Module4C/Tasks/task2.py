e3fcbb7761dc29f4f4da88b8cb29e52de4499a81
Mary <mary@moralis.io>
2024-05-08 15:25:28 +0100

    Task2

---
+++ Module4C/Tasks/task2.py
@@ -0,0 +1,13 @@
+"""
+Lesson 2 Task
+
+Declare a variable with variable_name as my_var and value as 12.6
+Declare an integer variable with the value of 200
+Declare a String variable with the value of your name
+"""
+
+my_var = 12.6
+
+number_of_students = 200
+
+my_name = 'Mary Onuorah'
