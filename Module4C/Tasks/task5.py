8932b10d8283088021b984b5c0ee01db9db2bf2b
Mary <mary@moralis.io>
2024-05-10 11:01:25 +0100

    Task5

---
+++ Module4C/Tasks/task5.py
@@ -0,0 +1,16 @@
+"""
+Declare a variable with value 2
+Check if the variable is equal to 2, if true print out "Number is Two"
+Add an else block which prints out "Number is not Two"
+
+If the value of the declared variable is changed from two,
+it should print "Number is not Two" in the console, if it
+changed back to 2 it should print "Number is Two" in the console.
+"""
+
+age = 2
+
+if age == 2:
+    print("Age is Two")
+else:
+    print("Age is not Two")
