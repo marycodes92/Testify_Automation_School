8204f4c1882520b85e7a231dc31f72c0fe92258d
Mary <mary@moralis.io>
2024-05-10 10:29:09 +0100

    Task4

---
+++ Module4C/Tasks/task4.py
@@ -0,0 +1,12 @@
+"""
+Declare two numbers with any values
+Add or Subtract or Multiply the two numbers together
+Print out the value of the operation
+"""
+
+age = 24
+age2 = 3
+
+# Multiplication
+multiply = age * age2
+print(multiply)
