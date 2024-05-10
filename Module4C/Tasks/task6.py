4b3afcf570eb5d625a4b09575efe45316c7b4d5c
Mary <mary@moralis.io>
2024-05-10 11:41:42 +0100

    Task 6

---
+++ Module4C/Tasks/task6.py
@@ -0,0 +1,24 @@
+"""
+Declare two numbers with any values
+Check if the first number is greater than 50 and check if the second number is less than 100, if True print out "Evaluation is True"
+Add an else block that prints "Evaluation is False"
+Declare a string with value "python"
+Check if the first string is equal to "testify" or "python", if True prints out "Yay!! The string is a Testify Python".
+
+Use the logical and to check if the first number is greater than 50 and the second number is less than 100.
+"""
+
+number_of_teachers = 70
+number_of_students = 90
+
+if number_of_teachers > 50 and number_of_students < 100:
+    print("Evaluation is True")
+else:
+    print("Evaluation is False")
+
+
+# Question B
+language = "Python"
+
+if language == "testify" or language == "Python":
+    print("Yay!! The string is a Testify Python")
