f71082b8f8ed172db7d90243963ab7af8395a312
Mary <mary@moralis.io>
2024-05-10 10:01:17 +0100

    Task3

---
+++ Module4C/Tasks/Task3.py
@@ -0,0 +1,15 @@
+"""
+Declare a raw string
+Declare two strings with any value
+Concatenate the two string
+Print out the value of the concatenated strings
+"""
+
+sentence = r"Welcome to Testify academy \nWhere test automation is made easy. \n\t Want to give a try?"
+
+firstName = "Mary"
+surname = "Onuorah"
+
+fullname = firstName + " " + surname
+
+print(fullname)
