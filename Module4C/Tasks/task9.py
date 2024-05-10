fa80d7723afc666a15d5357713543f583b0db5c9
Mary <mary@moralis.io>
2024-05-10 15:26:24 +0100

    Task 9

--- Module4C/Tasks/task9.py
+++ Module4C/Tasks/task9.py
@@ -3,3 +3,15 @@ Create a function that prints out "Hello World"
 Create a function that does nothing, an empty function
 Invoke/call both functions
 """
+
+
+def my_first_function():
+    print("Hello World")
+
+
+def nothing_yet():
+    pass
+
+
+my_first_function()
+nothing_yet()
