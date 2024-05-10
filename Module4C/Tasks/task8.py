be1c6cc81d10b22cde768151e94bd77404333831
Mary <mary@moralis.io>
2024-05-10 14:37:26 +0100

    Task 8

--- Module4C/Tasks/task8.py
+++ Module4C/Tasks/task8.py
@@ -4,8 +4,17 @@ Use for loop to iterate from 0 to 10
 For each iteration print out "Number ", i
 
 If the i value is equal to 2 add the continue
-
-If the i value is equal to 8 add the break statement
+a
+If the i value is equal to 8 dd the break statement
 
 For instruction 2, to print out Number and i, use the statement in your loop, print("Number", i)
 """
+
+number = 10
+
+for i in range(number):
+    if i == 2:
+        continue
+    if i == 8:
+        break
+    print("Number ", i)
