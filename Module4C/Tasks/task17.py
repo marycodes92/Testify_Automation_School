d0e189a3e426eb94cdd968fa390ce73d1e8bb8a7
Mary <mary@moralis.io>
2024-05-29 11:26:46 +0100

    Task 17

---
+++ Module4C/Tasks/task17.py
@@ -0,0 +1,14 @@
+"""
+Create a class called Human
+Initialize the class
+"""
+
+class Human:
+    gender = "male"
+    complexion = "Dark"
+
+
+# initializing the class
+isaac = Human()
+
+print(isaac.gender)
