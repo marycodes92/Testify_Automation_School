a33b32d5f4640d73fc1c7c56fdf4c125f1b975a5
Mary <mary@moralis.io>
2024-05-30 17:19:00 +0100

    sort array

---
+++ Module4C/CodingChallenge/sort.py
@@ -0,0 +1,10 @@
+# Sort an array of strings in alphabetical order
+
+def sort_array(arr):
+    return sorted(arr)
+
+
+fruits = ["Watermelon", "Apple", "Cucumber", "Banana"]
+students = ['Mary', 'Chiamaka', 'Benson', 'Etiena', 'Vivian', 'Ogechi']
+print(sort_array(fruits))
+print(sort_array(students))
