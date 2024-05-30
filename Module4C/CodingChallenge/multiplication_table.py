3b58a3e33fc14d54297574aa6b7cc2dd1b1b3031
Mary <mary@moralis.io>
2024-05-30 16:56:35 +0100

    multiplication table

---
+++ Module4C/CodingChallenge/multiplication_table.py
@@ -0,0 +1,17 @@
+# Print a table containing multiplication tables
+
+def multiplication(x, y):
+    return x * y
+
+print('2 x 1 = ', multiplication(2,1))
+print('2 x 2 = ', multiplication(2,2))
+print('2 x 3 = ', multiplication(2,3))
+print('2 x 4 = ', multiplication(2,4))
+print('2 x 5 = ', multiplication(2,5))
+print('2 x 6 = ', multiplication(2,6))
+print('2 x 7 = ', multiplication(2,7))
+print('2 x 8 = ', multiplication(2,8))
+print('2 x 9 = ', multiplication(2,9))
+print('2 x 10 = ', multiplication(2,10))
+print('2 x 11 = ', multiplication(2,11))
+print('2 x 12 = ', multiplication(2,12))
